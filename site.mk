cff339998137697b20162454ddcf3d82f4e01422
ed <ed@gop-srv3>
2017-11-18 12:29:50 +0100

    some adjustments for upcoming 2017.2.x (site_seed, remote passwd enabled) generate GLUON_RELEASE id from git tag now

--- site.mk
+++ site.mk
@@ -236,8 +236,10 @@ GLUON_TLWDR4900_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_P
 #   opkg compare-versions "$1" '>>' "$2"
 #   to decide if a version is newer or not.
 
-# Firmware name pattern
-DEFAULT_GLUON_RELEASE := 2017.1.3beta-exp$(shell date '+%Y%m%d')
+## Firmware name pattern
+# we use git to tell us the most recent tag of our branch
+DEFAULT_GLUON_RELEASE := $(shell git describe --always --dirty=+ 2>/dev/null || echo unknown)$(if $(GLUON_BRANCH),($(GLUON_BRANCH)),)-$(shell date '+%Y%m%d')
+
 
 ##  GLUON_RELEASE
 #    call make with custom GLUON_RELEASE flag, to use your own release version scheme.
